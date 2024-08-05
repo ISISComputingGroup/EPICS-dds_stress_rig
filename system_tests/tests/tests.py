a6b63863c94510b52ad8bc74688d9de5f20ae202
Jack Doughty <jackb.doughty@btinternet.com>
2024-08-05 13:25:54 +0100

    run safe ruff fixes

--- system_tests/tests/tests.py
+++ system_tests/tests/tests.py
@@ -1,11 +1,10 @@
 import unittest
 
+from parameterized import parameterized
 from utils.channel_access import ChannelAccess
 from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
-from utils.testing import get_running_lewis_and_ioc, skip_if_recsim, skip_if_devsim
-from parameterized import parameterized
-
+from utils.testing import get_running_lewis_and_ioc, skip_if_devsim
 
 DEVICE_PREFIX = "DDSSTRES_01"
 
